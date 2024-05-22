9c46a53ff8e93feb53e67347b16a86b98adbc7ff
bruno <brunotfatima@gmail.com>
2024-05-22 20:31:33 +0100

    added prettier on save and search view

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,25 @@
-import ResponsiveAppBar from "../../components/Navbar/ResponsiveAppBar";
+import { TextField } from '@mui/material';
+import ResponsiveAppBar from '../../components/Navbar/ResponsiveAppBar';
+import classes from './Home.module.css';
 
-type Props = {}
+type Props = {};
 
 const Home = (props: Props) => {
-    return <ResponsiveAppBar />
-}
+  return (
+    <>
+      <ResponsiveAppBar />
+      <div className={classes.container}>
+        <ul>
+          <li>
+            <h1>Search</h1>
+          </li>
+          <li>
+            <TextField id="outlined-basic" label="Search" variant="outlined" />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Home;
